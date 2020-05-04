4f9d1e811db1da64d802b362d8e46dd873f42489
Christian Henry <chenry5724@gmail.com>
2020-05-04 14:28:43 -0500

    pruning

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var path = require("path");
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = process.env.PORT;
+var PORT = process.env.PORT|| 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
@@ -38,10 +38,7 @@ var characters = [
   }
 ];
 
-// Routes
-// =============================================================
 
-// Basic route that sends the user first to the AJAX Page
 app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "view.html"));
 });
@@ -50,12 +47,11 @@ app.get("/add", function(req, res) {
   res.sendFile(path.join(__dirname, "add.html"));
 });
 
-// Displays all characters
 app.get("/api/characters", function(req, res) {
   return res.json(characters);
 });
 
-// Displays a single character, or returns false
+
 app.get("/api/characters/:character", function(req, res) {
   var chosen = req.params.character;
 
@@ -70,14 +66,11 @@ app.get("/api/characters/:character", function(req, res) {
   return res.json(false);
 });
 
-// Create New Characters - takes in JSON input
+
 app.post("/api/characters", function(req, res) {
-  // req.body hosts is equal to the JSON post sent from the user
-  // This works because of our body parsing middleware
+
   var newcharacter = req.body;
 
-  // Using a RegEx Pattern to remove spaces from newCharacter
-  // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
   newcharacter.routeName = newcharacter.name.replace(/\s+/g, "").toLowerCase();
 
   console.log(newcharacter);
@@ -87,8 +80,6 @@ app.post("/api/characters", function(req, res) {
   res.json(newcharacter);
 });
 
-// Starts the server to begin listening
-// =============================================================
 app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);
 });
